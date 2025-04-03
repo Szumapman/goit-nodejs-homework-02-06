6a57fede666524cbf789af1229e627909a5f2f6d
Paweł Szumański <pawel.szumanski@outlook.com>
2025-04-03 16:42:30 +0200

    add logic to route /:contactId

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -14,7 +14,16 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      return res.status(404).json({ message: 'Not found' });
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.post('/', async (req, res, next) => {
