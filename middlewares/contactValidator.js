81fe3b7c69a5d095c4624c21b9530b84ee00ce2c
Paweł Szumański <pawel.szumanski@outlook.com>
2025-04-04 10:38:45 +0200

    add a more human-friendly message when the phone contains invalid characters

--- middlewares/contactValidator.js
+++ middlewares/contactValidator.js
@@ -1,6 +1,7 @@
 const joi = require('joi');
 
 const contactValidator = (req, res, next) => {
+    MIN_NAME_LENGTH = 3;
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
         const requiredFields = ["name", "email", "phone"];
@@ -9,12 +10,15 @@ const contactValidator = (req, res, next) => {
         return res.status(400).json({ message: message });
     }
     const schema = joi.object({ // required() for all fields can be removed, as long as I check the missing fields above
-        name: joi.string().min(3).required(), 
+        name: joi.string().min(MIN_NAME_LENGTH).required(), 
         email: joi.string().email().required(),
         phone: joi.string().pattern(/^\(?\+?\d{1,4}?\)?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}$/).required(),
     });
     const { error } = schema.validate(req.body);
     if (error) {
+        if (error.message.includes("phone") && error.message.includes("pattern")) {
+            return res.status(400).json({ message: "Phone number must be digits and can contain spaces, dashes, parentheses and can start with +" });
+        }
         return res.status(400).json({ message: error.message });
     }
     next();
