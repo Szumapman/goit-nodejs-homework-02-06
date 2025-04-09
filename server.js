380709bd20c95190d08aac507e449b9c163e948a
Paweł Szumański <pawel.szumanski@outlook.com>
2025-04-09 19:19:45 +0200

    add connection to db

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app');
+require('dotenv').config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.connect(process.env.DB_HOST).then((db) => {
+  console.log("Database connection successful");
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000");
+  })
+}).catch(error => {
+  console.error("Database connection error: ", error.message);
+  process.exit(1);
 })
