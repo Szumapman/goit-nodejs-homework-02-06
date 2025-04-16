75ce99b82fd47d16dc6fbaa2c37be85cb56e0b26
Paweł Szumański <pawel.szumanski@outlook.com>
2025-04-16 21:42:22 +0200

    refactor

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const auth = (req, res, next) => {
     passport.authenticate('jwt', { session: false }, (err, user) => {
         if (err || !user) {
-            return res.status(401).json({ message: 'Unauthorized' });
+            return res.status(401).json({ message: 'Not authorized' });
         }
         req.user = user;
         next();
