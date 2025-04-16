100fa3048d88d19af25e0052b5269153d1a5e11f
Paweł Szumański <pawel.szumanski@outlook.com>
2025-04-16 22:10:25 +0200

    add get current user

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const signup = require('../../middlewares/routes/users/signup');
 const login = require('../../middlewares/routes/users/login');
 const logout = require('../../middlewares/routes/users/logout');
 const auth = require('../../middlewares/auth/auth');
+const getCurrentUser = require('../../middlewares/routes/users/getCurrentUser');
 
 const router = express.Router()
 
@@ -13,5 +14,7 @@ router.post('/signup', userValidator, signup);
 router.post('/login', userValidator, login);
 
 router.get('/logout', auth, logout);
+
+router.get('/current', auth, getCurrentUser);
 
 module.exports = router
