d968983e367f3236301b532002e2b1ba73a4b891
Paweł Szumański <pawel.szumanski@outlook.com>
2025-04-24 12:58:14 +0200

    add setup folders

---
+++ constants/folders.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const PUBLIC_DIR_NAME = 'public';
+const AVATARS_DIR_NAME = 'avatars';
+const TEMP_DIR_NAME = 'temp';
+
+const PUBLIC_PATH = path.join(process.cwd(), PUBLIC_DIR_NAME);
+const PUBLIC_AVATARS_PATH = path.join(process.cwd(), PUBLIC_DIR_NAME, AVATARS_DIR_NAME);
+const TEMP_AVATARS_PATH = path.join(process.cwd(), TEMP_DIR_NAME, AVATARS_DIR_NAME);
+
+module.exports = { PUBLIC_PATH, PUBLIC_AVATARS_PATH, TEMP_AVATARS_PATH };
+
