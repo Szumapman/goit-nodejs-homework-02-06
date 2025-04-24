1d2b2444b163b58ea5bb2e10f6f2ec9c19b021a8
Paweł Szumański <pawel.szumanski@outlook.com>
2025-04-25 01:00:07 +0200

    Create images.js

---
+++ constants/images.js
@@ -0,0 +1,4 @@
+const IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif'];
+const IMAGE_MIMETYPES = ['image/jpeg', 'image/png', 'image/gif'];
+
+module.exports = { IMAGE_EXTENSIONS, IMAGE_MIMETYPES };
