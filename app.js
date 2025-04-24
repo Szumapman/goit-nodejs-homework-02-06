f83e42cea69970437aca5e42be128fc79a351504
Paweł Szumański <pawel.szumanski@outlook.com>
2025-04-24 12:57:58 +0200

    add path to static resourcess

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const contactsRouter = require('./routes/api/contacts');
 const usersRouter = require('./routes/api/users');
 const jwtStrategy = require('./config/jwt');
+const { PUBLIC_PATH } = require('./constants/folders');
 
 const app = express();
+
+app.use(express.static(PUBLIC_PATH));
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
